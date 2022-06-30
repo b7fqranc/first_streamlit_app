d09d6acf717f104371da381b01b32f1fe53ea314
b7fqranc <108459287+b7fqranc@users.noreply.github.com>
2022-06-30 10:21:32 +0100

    Update streamlit_app.py
    Added macros

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title("My Parents new Healthy Diner")
 
@@ -7,3 +10,6 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket smoothie')
 streamlit.text('🐔 Hard-Boiled Free range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+
+streamlit.dataframe(my_fruit_list)
